6905cfae4eaa6531e82b5d6165afc4058fb0c2c6
Skaidra Bugenyte <s.bugenyte@gmail.com>
2022-02-07 10:56:06 +0200

    explanation different to my way of doing added as comments

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -4,6 +4,14 @@ import mainContext from "../context/mainContext";
 
 const SingleProductPage = () => {
 
+    // cia uztektu naudoti props o ne context, siuncama per viena lygi.
+    // daryta kitaip nei as dariau, import useParams from react-router-dom;
+    // const {url} = useParams()
+    // const product = getProduct.find(x => x.url === url) - sukurtu produktu array surask toki produkta, kurio url atitinka ta is params.
+    // atvaizdavimui sukure nauja komponenta singleProductView
+    // <singleProductView product={product}/>, siame komponente laukeliai nukopijuoti tokie patys, kaip ProductCard return.
+
+
     const {getSelected} = useContext(mainContext)
 
     return (
